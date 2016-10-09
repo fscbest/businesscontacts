5b75432537a127792d15c8f7951eb1083b854977
fscbest <romangeyber@gmail.com>
2016-10-09 18:00:21 +0300

    Frontend UI with Foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ import { Category } from './Category';
 export class AppComponent implements OnInit{
   businesses: Business[];
   categories: Category[];
+  appState: string;
+  activeKey: string;
 
   constructor(private _firebaseService: FirebaseService) {
 
@@ -26,5 +28,14 @@ export class AppComponent implements OnInit{
     this._firebaseService.getCategories().subscribe(categories => {
       this.categories = categories;
     });
+  }
+
+  changeState(state, key){
+    console.log("Changing state to: " + state);
+    if(key){
+      console.log("Changing key to " + key);
+      this.activeKey = key;
+    }
+    this.appState = state;
   }
 }
